13522d514651b72b061c44485d438de59c532ec5
Marta223 <mronconi6@gmail.com>
2026-09-24 14:47:58 +0100

    Add Capacitor and Android project (com.babsport.app)

---
+++ android/app/src/main/java/com/babsport/athletes/MainActivity.java
@@ -0,0 +1,5 @@
+package com.babsport.athletes;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.babsport.athletes',
+  appName: 'BAB',
+  webDir: 'dist'
+};
+
+export default config;
